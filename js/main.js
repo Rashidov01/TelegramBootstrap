49c16532ff42d07d78e4b50207428a5db7115563
Abdulloh <72493727+Rashidov01@users.noreply.github.com>
2021-08-21 17:45:32 +0500

    all done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const infoPage = document.querySelector('.info-page');
+const stickerOpener = document.querySelector(".sticker-opener");
+const StickerPage = document.querySelector('.sticker-page');
+
+stickerOpener.addEventListener('click', ()=> {
+  infoPage.classList.toggle('sr-only')
+})
+
+
+
